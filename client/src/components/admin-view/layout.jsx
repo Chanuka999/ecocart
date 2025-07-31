006ccee356385a7f0e8cf4a6e615f2920d190775
Chanuka999 <chanukaranditha99@gmail.com>
2025-07-31 23:37:33 +0530

    feat: implement AdminLayout component structure

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,5 +1,15 @@
+import { Outlet } from "react-router-dom";
+
 const AdminLayout = () => {
-  return <div></div>;
+  return (
+    <div className="flex min-h-screen w-full">
+      <div className="flex fkex-1 flex-col">
+        <main className="flex-1 flex bg-muted/40 p-4 md:p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 };
 
 export default AdminLayout;
