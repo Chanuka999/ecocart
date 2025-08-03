d909fa3a8452aa705c4fb77266f1820eb270a830
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-04 01:24:10 +0530

    feat: add AdminHeader and AdminSideBar components to AdminLayout

--- client/src/components/admin-view/layout.jsx
+++ client/src/components/admin-view/layout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import AdminSideBar from "./sidebar";
+import AdminHeader from "./header";
 
 const AdminLayout = () => {
   return (
     <div className="flex min-h-screen w-full">
+      <AdminSideBar />
       <div className="flex fkex-1 flex-col">
+        <AdminHeader />
         <main className="flex-1 flex bg-muted/40 p-4 md:p-6">
           <Outlet />
         </main>
