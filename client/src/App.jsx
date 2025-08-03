4299661a6ba1b0c9b16e338c83c48d1cf71534f8
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-04 01:23:59 +0530

    feat: add admin and shopping routes to App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,39 @@ import { Route, Routes } from "react-router-dom";
 import AuthRegister from "./pages/auth/register";
 import AuthLogin from "./pages/auth/login";
 import AuthLayout from "./components/auth/layout";
+import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminProducts from "./pages/admin-view/products";
+import AdminOrders from "./pages/admin-view/order";
+import AdminFeatures from "./pages/admin-view/features";
+import ShoppingLayout from "./components/shopping-view/layout";
+import NotFound from "./pages/not-found";
+import ShoppingAccount from "./pages/shopping-view/account";
+import ShoppingCheckout from "./pages/shopping-view/checkout";
+import ShoppingListing from "./pages/shopping-view/listing";
+import ShoppingHome from "./pages/shopping-view/home";
 
 function App() {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
-      <h1>Header component</h1>
-
       <Routes>
         <Route path="/auth" element={<AuthLayout />}>
           <Route path="login" element={<AuthLogin />} />
           <Route path="register" element={<AuthRegister />} />
         </Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="features" element={<AdminFeatures />} />
+        </Route>
+        <Route path="/shop" element={<ShoppingLayout />}>
+          <Route path="home" element={<ShoppingHome />} />
+          <Route path="listing" element={<ShoppingListing />} />
+          <Route path="checkout" element={<ShoppingCheckout />} />
+          <Route path="account" element={<ShoppingAccount />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
