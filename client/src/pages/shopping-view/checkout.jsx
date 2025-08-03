757f07182de6994a0355ca5af597cce1d9012c67
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-04 01:24:45 +0530

    feat: add NotFound, ShoppingCheckout, ShoppingHome, and ShoppingListing components for respective views

---
+++ client/src/pages/shopping-view/checkout.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShoppingCheckout = () => {
+  return <div>shopping view checkout</div>;
+};
+
+export default ShoppingCheckout;
---
+++ client/src/pages/shopping-view/listing.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShoppingListing = () => {
+  return <div>shopping view listing</div>;
+};
+
+export default ShoppingListing;
