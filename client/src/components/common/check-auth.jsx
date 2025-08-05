baccf412dabb15316ef6d4ede53c9c04e61980cc
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-06 00:02:45 +0530

    feat: enhance CheckAuth component to handle authentication redirects for users and admins

--- client/src/components/common/check-auth.jsx
+++ client/src/components/common/check-auth.jsx
@@ -1,9 +1,31 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 const CheckAuth = ({ isAuthenticated, user, children }) => {
   const location = useLocation();
-  return <div></div>;
+
+  if (
+    !isAuthenticated &&
+    !(
+      location.pathname.includes("/login") ||
+      location.pathname.includes("/regiter")
+    )
+  ) {
+    return <Navigate to="/auth/login" />;
+  }
+  if (
+    !isAuthenticated &&
+    !(
+      location.pathname.includes("/login") ||
+      location.pathname.includes("/register")
+    )
+  ) {
+    if (user?.role === "admin") {
+      return <Navigate to="/admin/dashboard" />;
+    } else {
+      return <Navigate to="/shop/home" />;
+    }
+  }
 };
 
 export default CheckAuth;
