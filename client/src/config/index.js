0659d9656adf13ed91553feba598bea35247c3be
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-10 00:38:29 +0530

    feat: add registerFormControls configuration for user registration form

---
+++ client/src/config/index.js
@@ -0,0 +1,23 @@
+export const registerFormControls = [
+  {
+    name: "userName",
+    lebel: "User Name",
+    placeholder: "Énter your user name",
+    componentType: "input",
+    type: "text",
+  },
+  {
+    name: "email",
+    lebel: "Email",
+    placeholder: "Énter your email",
+    componentType: "input",
+    type: "email",
+  },
+  {
+    name: "password",
+    lebel: "password",
+    placeholder: "Énter your user password",
+    componentType: "input",
+    type: "password",
+  },
+];
