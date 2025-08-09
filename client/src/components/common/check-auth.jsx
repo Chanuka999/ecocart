65e5b26112e938f39d12474a2d6c54a0f85cddcb
Chanuka999 <chanukaranditha99@gmail.com>
2025-08-09 23:58:19 +0530

    feat: update CheckAuth component to handle admin route access and redirect users accordingly

--- client/src/components/common/check-auth.jsx
+++ client/src/components/common/check-auth.jsx
@@ -26,6 +26,22 @@ const CheckAuth = ({ isAuthenticated, user, children }) => {
       return <Navigate to="/shop/home" />;
     }
   }
+  if (
+    isAuthenticated &&
+    user?.role !== "admin" &&
+    location.pathname.includes("admin")
+  ) {
+    return <Navigate to="/unauth-page" />;
+  }
+
+  if (
+    isAuthenticated &&
+    user?.role === "admin" &&
+    location.pathname.includes("shop")
+  ) {
+    return <Navigate to="/admin/dashboard" />;
+  }
+  return <>{children}</>;
 };
 
 export default CheckAuth;
