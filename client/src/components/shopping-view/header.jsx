9088d0e92ce1e4558df48a24589aae4a60915c70
Chanuka999 <chanukaranditha99@gmail.com>
2025-09-29 19:04:31 +0530

    update

--- client/src/components/shopping-view/header.jsx
+++ client/src/components/shopping-view/header.jsx
@@ -1,25 +1,65 @@
-import { useDispatch } from "react-redux";
-import { logoutUser } from "../../../store/auth-slice";
-import { useNavigate } from "react-router-dom";
+import { HousePlug, Menu } from "lucide-react";
+import { Link } from "react-router-dom";
+import { Sheet, SheetTrigger, SheetContent } from "../ui/sheet";
 import { Button } from "../ui/button";
+import { useSelector } from "react-redux";
+import { shoppingViewHeaderMenuItems } from "../../config";
+import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
+
+const MenuItem = () => {
+  return (
+    <nav className="flex flex-col mb-3 lg:mb-0 lg:items-center gap-6 lg:flex-row">
+      {shoppingViewHeaderMenuItems.map((menuItem) => (
+        <Link
+          className="text-sm font-medium"
+          key={menuItem.id}
+          to={menuItem.path}
+        >
+          {menuItem.label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+const HeaderRightContent = () => {
+  return (
+    <div className="flex lg:items-center lg:flex-row flex-col gap-4">
+      <Button variant="outline" size="icon">
+        <ShoppingCart className="w-6 h-6" />
+        <span className="sr-only">User cart</span>
+      </Button>
+      <DropdownMenu> </DropdownMenu>
+    </div>
+  );
+};
 
 const ShoppingHeader = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    dispatch(logoutUser()).then(() => {
-      navigate("/auth/login");
-    });
-  };
-
+  const { isAuthenticated } = useSelector((state) => state.auth);
   return (
-    <div className="flex items-center justify-between p-4 bg-gray-100 w-full">
-      <div>shopping view header</div>
-      <Button variant="outline" onClick={handleLogout}>
-        Logout
-      </Button>
-    </div>
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="flex h-16 items-center justify-between px-4 md:px-6">
+        <Link to="/shop/home" className="flex items-center gap-2">
+          <HousePlug className="h-6 w-6" />
+          <span className="font-bold">Ecommerce</span>
+        </Link>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant="outline" size="icon" className="lg:hidden">
+              <Menu className="h-6 w-6" />
+              <span className="sr-only">Toggle header menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left" className="w-full max-w-xs">
+            <MenuItem />
+          </SheetContent>
+        </Sheet>
+        <div className="hidden lg:block">
+          <MenuItem />
+        </div>
+        {isAuthenticated ? <div></div> : null}
+      </div>
+    </header>
   );
 };
 
