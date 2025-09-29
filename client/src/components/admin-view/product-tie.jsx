dd4d859df941530b6bd40b9e9b8b0bff9051424b
Chanuka999 <chanukaranditha99@gmail.com>
2025-09-29 19:04:17 +0530

    update

--- client/src/components/admin-view/product-tie.jsx
+++ client/src/components/admin-view/product-tie.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import { Card, CardContent, CardFooter } from "../ui/card";
 import { Button } from "../ui/button";
 
-const AdminProductTile = ({ product }) => {
+const AdminProductTile = ({
+  product,
+  setOpenCreateProductsDialog,
+  setCurrentEditedId,
+  setFormData,
+  handleDelete,
+}) => {
   return (
     <Card className="w-full max-w-sm mx-auto">
       <div>
@@ -29,8 +35,16 @@ const AdminProductTile = ({ product }) => {
           </div>
         </CardContent>
         <CardFooter className="flex justify-between items-center">
-          <Button>Edit</Button>
-          <Button>Delete</Button>
+          <Button
+            onClick={() => {
+              setOpenCreateProductsDialog(true);
+              setCurrentEditedId(product?._id);
+              setFormData(product);
+            }}
+          >
+            Edit
+          </Button>
+          <Button onClick={() => handleDelete(product?._id)}>Delete</Button>
         </CardFooter>
       </div>
     </Card>
