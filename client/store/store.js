e8851316d58619710ffb9d45402780876d99400b
Chanuka999 <chanukaranditha99@gmail.com>
2025-10-01 10:27:17 +0530

    fix: correct import path for shoppingProductSlice in store configuration

--- client/store/store.js
+++ client/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth-slice/index.js";
 import adminProductsSlice from "./admin/product-slice/index.js";
-import shoppingProductSlice from "./shopping-slice/index.js";
+import shoppingProductSlice from "./shop/product-slice/index.js";
 
 const store = configureStore({
   reducer: {
