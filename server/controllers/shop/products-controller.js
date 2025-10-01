527951e04837d25f896361cc58320446fa3d7c1c
Chanuka999 <chanukaranditha99@gmail.com>
2025-10-01 10:27:38 +0530

    fix: implement filtering and sorting for product retrieval in getFilteredProducts

--- server/controllers/shop/products-controller.js
+++ server/controllers/shop/products-controller.js
@@ -1,8 +1,42 @@
-import Product from "../../models/Product";
+import Product from "../../models/Product.js";
 
 const getFilteredProducts = async (req, res) => {
   try {
-    const products = await Product.find({});
+    const { category = [], brand = [], sortBy = "price-lowtohigh" } = req.query;
+
+    let filter = {};
+
+    if (category.length) {
+      filter.category = { $in: category.split(",") };
+    }
+
+    if (brand.length) {
+      filter.category = { $in: category.split(",") };
+    }
+
+    let sort = {};
+    switch (sortBy) {
+      case "price-lowtohigh":
+        sort.price = 1;
+        break;
+
+      case "price-hightolow":
+        sort.price = -1;
+        break;
+
+      case "title-atoz":
+        sort.title = 1;
+        break;
+
+      case "title-ztoa":
+        sort.title = -1;
+        break;
+
+      default:
+        sort.price = 1;
+        break;
+    }
+    const products = await Product.find(filter).sort(sort);
     res.status(200).json({ success: true, data: products });
   } catch (error) {
     console.log(error);
