340a97e930ef836a4890478699bdb46b80f979d8
Chanuka999 <chanukaranditha99@gmail.com>
2025-10-03 23:22:17 +0530

    update

--- server/helpers/paypal.js
+++ server/helpers/paypal.js
@@ -1,11 +1,12 @@
 import paypal from "paypal-rest-sdk";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 paypal.configure({
   mode: "sandbox",
-  client_id:
-    "AUze0uCKYTPO3s5pfSj1GF5gUFVNHETKzy5i-YGJ_-MYmJgugLfMkijS88j3kxYB4xlErY_SvzfxVpz_",
-  client_server:
-    "EPTa9KpKQ5Rc3_8FNw4cTnJuAMFTBGCYvzfZvgppZkx710n4o0RRM0OpP5sswW4cA4aA4GPW89kmpSgj",
+  client_id: process.env.CLIENT_ID,
+  client_server: process.env.CLIENT_SERVER,
 });
 
 export default paypal;
