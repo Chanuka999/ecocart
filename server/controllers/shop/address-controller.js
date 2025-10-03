1f63c8a6a146c6079e7f9c4d4326b81ff425d456
Chanuka999 <chanukaranditha99@gmail.com>
2025-10-03 16:17:25 +0530

    fix: update import statement and module export syntax in address-controller

--- server/controllers/shop/address-controller.js
+++ server/controllers/shop/address-controller.js
@@ -1,4 +1,4 @@
-import Address from "../../models/Address";
+import Address from "../../models/Address.js";
 
 const addAddress = async (req, res) => {
   try {
@@ -133,4 +133,4 @@ const deleteAddress = async (req, res) => {
   }
 };
 
-module.exports = { addAddress, editAddress, fetchAllAddress, deleteAddress };
+export { addAddress, editAddress, fetchAllAddress, deleteAddress };
