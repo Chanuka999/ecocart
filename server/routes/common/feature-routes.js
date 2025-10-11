1d9aa22ee9edd7a1a3eb2db765ea58ef436cbed6
Chanuka999 <chanukaranditha99@gmail.com>
2025-10-11 13:29:58 +0530

    feat: add feature routes for image management

---
+++ server/routes/common/feature-routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import {
+  addFeatureImage,
+  getFeatureImage,
+  deleteFeatureImage,
+} from "../../controllers/common/Feature-controller.js";
+
+const router = express.Router();
+
+router.post("/add", addFeatureImage);
+router.get("/get", getFeatureImage);
+router.delete("/delete/:id", deleteFeatureImage);
+
+export default router;
